b7a11ac628d46d8b6f62ee14036229782ef1e791
Ich <info@anito.de>
2015-03-26 20:54:43 +0100

    Revert "init"
    This reverts commit 5e50f3f974c5503987513d9cffad50f23e72d8a8.

--- storage/configuration/key.php
+++
@@ -1,6 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-// Do not edit or remove this file unless advised by Koken support
-// support@koken.me
-
-$KOKEN_ENCRYPTION_KEY = 'fd9fc85c5d76ccc7f0fc52d244a2d367';
